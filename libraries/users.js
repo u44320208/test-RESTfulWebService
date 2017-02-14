83a3dd6902e24018a0de7e2343f81a3e8e011e05
Ch PP <u44320208@gmail.com>
2017-02-14 21:49:21 +0700

    Change Persistence data to json file

--- libraries/users.js
+++ libraries/users.js
@@ -1,42 +1,98 @@
-var users = [
-    {
-        "id": 1,
-        "username": "goldroger",
-        "name": "Gol D. Roger",
-        "position": "Pirate King"
-    },
-    {
-        "id": 2,
-        "username": "mrzero",
-        "name": "Sir Crocodile",
-        "position": "Former-Shichibukai"
-    },
-    {
-        "id": 3,
-        "username": "luffy",
-        "name": "Monkey D. Luffy",
-        "position": "Captain"
-    },
-    {
-        "id": 4,
-        "username": "kuzan",
-        "name": "Aokiji",
-        "position": "Former Marine Admiral"
-    },
-    {
-        "id": 5,
-        "username": "shanks",
-        "name": "'Red-Haired' Shanks",
-        "position": "The 4 Emperors"
-    }
-];
+var fs = require("fs");
 
-exports.findAll = function() {
-    return users;
+var myFile = 'users.json'; 
+
+//********* Support ES5 (IE9+) *********
+// If you can't , you could also use the jQuery (http://api.jquery.com/jQuery.map/) 
+// or Underscore.js (http://underscorejs.org/#map) implementations.
+// type : 'max' or 'min'
+// data : 
+function getMinMax(type, data, key) {
+	var propValues;
+	propValues = data.map(function(node) {
+		return node[key];
+	  });
+	if(type !== 'min'){
+		return Math.max.apply(null, propValues);
+	}else{
+		return Math.min.apply(null, propValues);
+	}
+}
+//*************************************
+
+//********* Find for all User *********
+exports.findAll = function(callbackError,callbackSuccess) {
+	fs.readFile(myFile, 'utf8', function (err, data) {
+		if (err) {
+			callbackError(error);
+		}else{
+			data = JSON.parse(data);
+			callbackSuccess(data);
+		}
+   });
 };
 
-exports.findById = function (id) {
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].id == id) return users[i];
-    }
+//********* Find User By ID *********
+exports.findById = function (id, callbackError, callbackSuccess) {
+    fs.readFile(myFile, 'utf8', function (err, data) {
+	    if (err) {
+			callbackError(err);
+		}else{
+			var foundData;
+			data = JSON.parse(data);
+			foundData = data.find(d => d.id == id);
+			callbackSuccess(foundData);
+		}
+   });
+};
+
+//********* Add new User *********
+exports.newUser = function(newuser, callbackError, callbackSuccess) {
+	fs.readFile(myFile, 'utf8', function (err, data) {
+	    if (err) {
+			callbackError(err);
+		}else{
+			data = JSON.parse(data);
+			var maxId = getMinMax('max', data, 'id');
+			newuser.id = maxId + 1;
+			data.push(newuser);
+			fs.writeFile(myFile, JSON.stringify(data), function(err){
+			  if (err){
+				  callbackError(err);
+			  }else{
+				  console.log('Success Add new User ID : '+newuser.id);
+				  callbackSuccess(newuser);
+			  }
+			});
+		}
+   });
+};
+
+//********* Edit User *********
+exports.editUser = function(id, edituser, callbackError, callbackSuccess) {
+	fs.readFile(myFile, 'utf8', function (err, data) {
+	    if (err) {
+			callbackError(err);
+		}else{
+			data = JSON.parse(data);
+			foundData = data.find(d => d.id == id);
+			// add code here
+
+		}
+   });
+};
+
+//********* Drop User *********
+exports.dropUser = function (id, callbackError, callbackSuccess) {
+    fs.readFile(myFile, 'utf8', function (err, data) {
+	    if (err) {
+			callbackError(err);
+		}else{
+			var foundData;
+			data = JSON.parse(data);
+			foundData = data.find(d => d.id == id);
+			// add code to drop user by id here
+
+		}
+   });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var users = require('../library/users');
+var users = require('../libraries/users');
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -13,19 +13,65 @@ router.get('/index', function (req, res) {
     res.send('<h1>This is index page</h1>');
 });
 
+//********* Show all User *********
 router.get('/user', function (req, res) {
-    res.json(users.findAll());
+	users.findAll(function(errorMessage){
+        console.log(errorMessage);
+        res.end("Error");
+    }, function(data){
+        res.json(data);
+    });
 });
 
+//********* Show User by ID *********
 router.get('/user/:id', function (req, res) {
     var id = req.params.id;
-    res.json(users.findById(id));
+    users.findById(id, function(errorMessage){
+        console.log(errorMessage);
+        res.end("Error");
+    }, function(data){
+        res.json(data);
+    });
 });
 
+//********* Add new User *********
 router.post('/newuser', function (req, res) {
-    var json = req.body;
-	console.log(json.name);
-    res.send('Add new ' + json.name + ' Completed!');
+    var newuser = req.body;
+	users.newUser(newuser,function(errorMessage){
+        console.log(errorMessage);
+        res.end("Error");
+    }, function(newuser){
+        //res.send('<h1>Add new User '+newuser.name+' Success</h1>');
+		res.redirect("user");
+    });
 });
+
+//********* Edit User by post *********
+router.post('/user/:id/edit', function (req, res) {
+	var id = req.params.id;
+	var editData = req.body;
+	// add code here
+	users.dropUser(id, editData, function(errorMessage){
+        console.log(errorMessage);
+        res.end("Error");
+    }, function(data){
+        res.redirect("user/"+id);
+    });
+
+});
+
+//********* Drop User by get *********
+router.get('/user/:id/drop', function (req, res) {
+	var id = req.params.id;
+	// add code here
+	users.dropUser(id, function(errorMessage){
+        console.log(errorMessage);
+        res.end("Error");
+    }, function(data){
+		//res.send('<h1>Droup User '+data.name+' Success</h1>');
+        res.redirect("user");
+    });
+});
+
 
 module.exports = router;
